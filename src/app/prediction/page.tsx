46fc7f9b20816b695c0d0ae96f9836c584aa3a96
Dinesh Jangid <spideyyyyy89@gmail.com>
2024-06-02 23:56:42 +0530

    basic-folder-strc

---
+++ src/app/prediction/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FormPage from '@/components/FormPage'
+
+
+const PredictionPage = () => {
+  return (
+    <div>
+        prediction-page
+        <FormPage/>
+    </div>
+  )
+}
+
+export default PredictionPage
